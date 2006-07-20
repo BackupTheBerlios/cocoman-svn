ec6dbf13fa1ba51d2782f96fde0ee8f61ef62200
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-07-20 05:41:03 +0000

    No longer show valid xhtml graphic cause we keep reloading the page. dbenamy | 2006-02-11 03:06:07 -0500 (Sat, 11 Feb 2006)

--- web/footer.php
+++ web/footer.php
@@ -1,8 +1,6 @@
   <hr />
   <p>
-    <a href="http://validator.w3.org/check?uri=referer"><img
-        src="http://www.w3.org/Icons/valid-xhtml10"
-        alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+    <a href="http://validator.w3.org/check?uri=referer">Valid XHTML 1.0 Transitional</a>
   </p>
 </body>
 </html>
