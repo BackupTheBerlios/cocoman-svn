53e58a3f4221a7a8d50e872ae39b8b1231773e3f
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-07-20 06:23:59 +0000

    Fixed it so the class loader works better with paths. And the jar can be used from anywhere. acmweb | 2006-04-21 11:03:18 -0400 (Fri, 21 Apr 2006)

--- trunk/source/javarun/javarun.java
+++ trunk/source/javarun/javarun.java
@@ -1,35 +1,46 @@
 import java.lang.reflect.*;
 import java.io.File;
 import java.util.StringTokenizer;
+import java.io.*;
+
 class JavaRun {
     public static void main(String[] args) {
         int i;
         File dotslash = new File("./");
         String[] contents = dotslash.list();
         String[] tokens;
+        Class defined;
         String classname="";
         Class[] argTypes = new Class[1];
         argTypes[0] = String[].class;
         Object[] parameters = new Object[1];
         parameters[0] = new String[0];
         Object[] pargs = parameters;
+        System.setProperty("java.class.path", ".");
         for (i = 0; i < contents.length; i++) {
             try {
+                File curfile = new File(contents[i]);
                 tokens = contents[i].split("\\.");
                 classname = "";
                 for (int j = 0; j < tokens.length-1; j++) {
                     classname += tokens[j];
                 }
-                if (classname.equals("Runner")) continue;
-                Method mainMethod = Class.forName(classname).getDeclaredMethod("main", argTypes);
+                if (classname.equals("JavaRun")) continue;
+                if (curfile.isDirectory()) continue;
+//                Method mainMethod = Class.forName(classname).getDeclaredMethod("main", argTypes);
+//                  Method mainMethod = loadClass(contents[i]).getDeclaredMethod("main", argTypes);
+                  defined = findClass(contents[i]);
+                  Method mainMethod = defined.getDeclaredMethod("main", argTypes);
+                  mainMethod.setAccessible(true);
                 try {
                     mainMethod.invoke(null, pargs);
                 } catch (IllegalAccessException e) {
 //                    System.out.println(e);
                 } catch (InvocationTargetException e) {
 //                    System.out.println(e);
+                } catch (ClassFormatError e) {
+//                    System.out.println(e); 
                 } finally { 
-                    break;
                 }
             } catch (ClassNotFoundException e) {
 //                System.out.println(e);
@@ -38,5 +49,62 @@ class JavaRun {
             }
         }
     }
+   
+
+    // Taken from Raymond DeCampo on http://www.thescripts.com/forum/thread15909.html 
+    static Class findClass(final String filename) throws
+        ClassNotFoundException
+        {
+            ClassLoader loader = new ClassLoader()
+            {
+                protected Class findClass(String name) throws
+                    ClassNotFoundException
+                    {
+                        // Always load the file
+                        InputStream in = null;
+                        try
+                        {
+                            in = new BufferedInputStream(new
+                                    FileInputStream(filename));
+                            ByteArrayOutputStream byteStr = new
+                                ByteArrayOutputStream();
+                            int byt = -1;
+                            while ((byt = in.read()) != -1)
+                            {
+                                byteStr.write(byt);
+                            }
+                            byte byteArr[] = byteStr.toByteArray();
+                            return defineClass(null, byteArr, 0, byteArr.length);
+                        }
+                        catch (final RuntimeException rex)
+                        {
+                            throw rex;
+                        }
+                        catch (final Exception ex)
+                        {
+                            ex.printStackTrace();
+                            throw new ClassNotFoundException(name);
+                        }
+                        catch (ClassFormatError ex) {
+                            throw new ClassNotFoundException(name);
+                        }
+                        finally
+                        {
+                            if (in != null)
+                            {
+                                try
+                                {
+                                    in.close();
+                                }
+                                catch (final IOException ioe)
+                                {
+                                    ioe.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+            };
+            return loader.loadClass("garbage");
+        }
 }
-            
+
