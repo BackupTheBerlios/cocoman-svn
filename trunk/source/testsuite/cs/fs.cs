702326859b2c063c578c21bb5df26fd26fdbd530
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-07-20 06:16:38 +0000

    Added a test for creating files. acmweb | 2006-03-09 00:49:32 -0500 (Thu, 09 Mar 2006)

---
+++ trunk/source/testsuite/cs/fs.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Drew
+{
+  class DeleteTest
+  {
+    static void Main(string[] args)
+    {
+      //try {
+        System.IO.FileInfo fi = new System.IO.FileInfo("somefile");
+      //} catch (System.IO.IOException e) {
+        //Console.WriteLine(e.Message);
+      //}
+    }
+  }
+}
