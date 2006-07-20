000c34653ef189bcae4a2b01c537588ada2b5fa0
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-07-20 05:48:35 +0000

    Simplifying scoreboard file stuff part 1. dbenamy | 2006-02-13 16:20:29 -0500 (Mon, 13 Feb 2006)

--- web/scoreboard.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php'); ?>
-<?ini_set("include_path", ".:../");?>
-<?php include('scores.php'); ?>
-<?php include('file-submission-form.php'); ?>
-<?php include('footer.php'); ?>
