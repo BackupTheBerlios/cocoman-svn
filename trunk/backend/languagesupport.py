0aab5f5781a0502ee68e4b3e67f6f817a9d9a3fd
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-08-18 06:32:25 +0000

    * Added test for LanguageSupport. * Starting work on LanguageSupport.

--- trunk/backend/languagesupport.py
+++ trunk/backend/languagesupport.py
@@ -3,13 +3,31 @@
 # Licensed under the GPLv2
 
 
+import threading
+from threading import Thread
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+
+class ExecutionTimedOutError(Exception):
+    pass
+
+
+class FailedTestError(Exception):
+    pass
+
+
+class CompileFailedError(Exception):
+    pass
+
+
 class LanguageSupport:
     """This is a base class which all classes that offer support for
     languages must derive from. This class can't be instantiated.
     """
     def __init__(self):
-        if type(self) == LanguageSupport:
+        if type(self) == "LanguageSupport":
             raise NotImplementedError
+            # TODO Fix this
     
     def get_supported_extensions(self):
         """Case of letters doesn't matter- extensions will be matched case 
@@ -33,4 +51,48 @@ class LanguageSupport:
         raise NotImplementedError
 
     def _run_commands_capture_output(self, commands):
-        raise NotImplementedError
+        """Runs the specified commands, kill them if the timeout is exceeded, 
+        and returns anything the program(s) output on stdout or stderr.
+        commands must be a list of commands to run where each command to run
+        is a list with the command to run in the 0th element followed by the 
+        args in the following positions. Eg:
+        [
+            ["ls"],
+            ["mv", "foo", "bar"]
+        ]
+        Can raise ExecutionTimedOutError.
+        Implementation: This function spawns a second thread to do the execution
+        of the commands and read their output into a class variable. Since that 
+        thread doesn't block on anything and reads both stdout and stderr in one
+        stream, there shouldn't be any possible deadlocks.
+        The first thread does the timing and kills the child process if the 
+        timeout specified in the settings is exceeded. Then it returns the data 
+        captured by the second thread.
+        """
+        assert type(commands) == list
+        assert len(commands) > 0
+        assert type(commands[0]) == list
+        assert len(commands[0]) > 0
+        self.commands_output = ""
+        # commands needs to be in [] because the call dereferences one set of brackets
+        run_commands_thread = Thread(target=self.__actual_run_commands_capture_output, args=[commands])
+        # TODO start timer
+        run_commands_thread.run()
+        # TODO kill process when timer expires
+        return self.commands_output
+    
+    def __actual_run_commands_capture_output(self, commands):
+        """Runs the specified commands and puts the output in 
+        self.commands_output.
+        commands must be a list of commands to run where each command to run
+        is a list with the command to run in the 0th element followed by the 
+        args in the following positions. Eg:
+        [
+            ["ls"],
+            ["mv", "foo", "bar"]
+        ]
+        """
+        for command in commands:
+            self.process = Popen(command, stdin=PIPE, stdout=PIPE, 
+                                    stderr=STDOUT, close_fds=True)
+            print "type of pipe is %s" % type(self.process.stdin)
---
+++ trunk/backend/test/testlanguagesupport.py
@@ -0,0 +1,27 @@
+import sys
+import os
+# Enable importing from parent directory
+relativePathToScript = os.path.split(sys.argv[0])[0]
+relativePathToParent = os.path.join(relativePathToScript, "..")
+sys.path.append(relativePathToParent)
+import unittest
+from settings import Settings
+import languagesupport
+from languagesupport import LanguageSupport
+
+
+class TestLanguageSupport(unittest.TestCase):
+    def setUp(self):
+        self.language_support = LanguageSupport()
+    
+    def testRunOneCommand(self):
+        output = self.language_support._run_commands_capture_output([["echo", "doody on my head"]])
+        self.assertEqual(output, "doody on my head")
+
+
+def suite():
+    # TODO
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
