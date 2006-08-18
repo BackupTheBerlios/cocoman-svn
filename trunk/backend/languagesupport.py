80f1d5c1f7d5aa7cb238fc8fad5ab1454ee690e6
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-08-18 02:22:26 +0000

    * Added comment to Language support and made most of the functions raise NotImplementedError.

--- trunk/backend/languagesupport.py
+++ trunk/backend/languagesupport.py
@@ -4,19 +4,25 @@
 
 
 class LanguageSupport:
-
+    """This is a base class which all classes that offer support for
+    languages must derive from. This class can't be instantiated.
+    """
+    def __init__(self):
+        if type(self) == LanguageSupport:
+            raise NotImplementedError
+    
     def get_supported_extensions(self):
         """Case of letters doesn't matter- extensions will be matched case 
         insensitively (all lower case recommended for consistancy).
         Should not include a '.' before the letters of the extension.
         """
-        pass
+        raise NotImplementedError
 
     def build(self, source_file_name):
         """Throws:
         CompileFailedError
         """
-        pass
+        raise NotImplementedError
 
     def run(self, executable_file_name):
         """Throws:
@@ -24,7 +30,7 @@ class LanguageSupport:
         FailedTestError?
         I don't know yet how piping in the input and grabbing the output will work.
         """
-        pass
+        raise NotImplementedError
 
     def _run_commands_capture_output(self, commands):
-        pass
+        raise NotImplementedError
