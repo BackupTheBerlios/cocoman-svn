76406bfa342e746044f4ec0dad6ece8b09259d84
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-08-26 07:03:30 +0000

    * Adding script which finds and runs all the tests.

---
+++ trunk/backend/test/testall.py
@@ -0,0 +1,32 @@
+"""This script searched for any module in this directory with the name test in 
+it. Then it looks for any classes in those modules that inherits from TestCase 
+and compiles a suite of all of them. Finally, it runs the suite.
+
+"""
+
+import sys
+import os
+# Enable importing from parent directory
+relative_path_to_script = os.path.split(sys.argv[0])[0]
+sys.path.append(relative_path_to_script)
+import unittest
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    dir_contents = os.listdir(os.path.join(relative_path_to_script, '.'))
+    for entry in dir_contents:
+        entry_with_path = os.path.join(relative_path_to_script, entry)
+        if os.path.isfile(entry_with_path) and entry.rsplit('.')[-1].lower() == 'py':
+            try:
+                module = __import__(entry[:-3])
+            except ImportError:
+                pass
+            for attribute in dir(module):
+                try:
+                    if issubclass(module.__dict__[attribute], unittest.TestCase):
+                        print "Adding %s to test suite." % attribute
+                        suite.addTest(unittest.makeSuite(module.__dict__[attribute]))
+                except TypeError:
+                    pass
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
