a788061b560e409b5db058e164018595b450e76a
dbenamy <dbenamy@131e760a-ea18-0410-9ca4-9943b865d30e>
2006-08-26 23:58:52 +0000

    * Added cleanup of allowed languages after each test. * I have no idea why testInvalidLanguage is failing.

--- trunk/backend/test/testsubmissionprocessor.py
+++ trunk/backend/test/testsubmissionprocessor.py
@@ -5,13 +5,17 @@ relativePathToScript = os.path.split(sys.argv[0])[0]
 relativePathToParent = os.path.join(relativePathToScript, "..")
 sys.path.append(relativePathToParent)
 import unittest
-from settings import settings, Settings
+from settings import settings
 from submissionprocessor import SubmissionProcessor, UnhandledExtensionError
 
 
 class TestSubmissionProcessor(unittest.TestCase):
     def setUp(self):
         self.submission_processor = SubmissionProcessor()
+    
+    def tearDown(self):
+        for language in settings.get_allowed_languages():
+            settings.remove_allowed_language(language)
     
     def testLoadLanguageSupport(self):
         settings.add_allowed_language('Cpp')
